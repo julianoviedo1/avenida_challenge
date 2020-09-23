b30732941812938b214449d658713e099bb8c366
julianoviedo <julianoviedo1@gmail.com>
2020-09-23 00:30:53 -0300

    mines solves in x and y

--- app/controllers/mine_maps_controller.rb
+++ app/controllers/mine_maps_controller.rb
@@ -1,12 +1,22 @@
 require 'json'
 require 'open-uri'
 class MineMapsController < ApplicationController
+
+  def index
+    @map = fetch_map
+  end
+
+  private
+
   def fetch_map
     url = 'https://mine-sweeper-generator.herokuapp.com/solver'
     user_serialized = open(url).read
-    user = JSON.parse(user_serialized)
+    JSON.parse(user_serialized)
+  end
+
+  def map_decompose(mapa)
+    # cada array representa una row, cada element lo reemplazo por 1 si es bomba
+    mapa.each { |row| row.replace(row.map { |element| element == '*' ? true : 0 }) }
+    mapa.last
   end
 end
-
-
-
